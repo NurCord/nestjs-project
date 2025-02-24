0fd051120ec2e603cb339c781e1ba9b35e028129
nurcord <nunyypin@gmail.com>
2025-02-23 22:09:18 -0300

    feat: add config file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
-    private configService: ConfigService,
     @Inject('TASKS') private tasks: any[],
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
+
   getHello(): string {
-    const apiKey = this.configService.get('API_KEY');
-    const databaseName = this.configService.get('DATABASE_NAME');
+    const apiKey = this.configService.apiKey;
+    const databaseName = this.configService.database.name;
     return `Hello World ${apiKey} ${databaseName}`;
   }
 }
